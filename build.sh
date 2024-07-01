de93143c49dde081a8729215d51a5670440834f8
Conradin Laux <conradin.laux@gmail.com>
2024-07-01 10:18:56 +0200

    chore: dont copy in the build to make neovim-agnostic

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ rm gobra.so
 
 tree-sitter build
 
-cp ./gobra.so ~/.config/nvim/parser/
-cp ./queries/highlights.scm ~/.config/nvim/queries/gobra/
+# cp ./gobra.so ~/.config/nvim/parser/
+# cp ./queries/highlights.scm ~/.config/nvim/queries/gobra/
